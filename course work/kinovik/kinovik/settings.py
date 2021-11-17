b80bac9478a1b68857c1032971d0cf26a47f75fd
ArtsiomKirychuk <artsiom.kirychuk@gmail.com>
2021-11-17 07:50:04 +0300

    mark up the page, added fontawesome

--- course work/kinovik/kinovik/settings.py
+++ course work/kinovik/kinovik/settings.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 import os
+import sys
+
+sys.modules['fontawesome_free'] = __import__('fontawesome-free')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'fontawesome_free',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -128,6 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'boot'),
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
